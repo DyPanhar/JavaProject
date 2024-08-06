daab76f52475c192c0de49342b35bab275307bc3
DyPanhar <142957658+DyPanhar@users.noreply.github.com>
2024-08-06 15:25:15 +0700

    Delete unecessary file

--- Project/VotinSystem/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.votinsystem {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.votinsystem to javafx.fxml;
-    exports com.example.votinsystem;
-}
